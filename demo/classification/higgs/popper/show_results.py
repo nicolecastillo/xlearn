a6490cea8a715bc5c63dcb87eba3a376a56cf930
Nicole Castillo <nicole.castillomac@gmail.com>
2020-07-01 15:04:47 -0700

    add figures in their respective folder

--- demo/classification/higgs/popper/show_results.py
+++ demo/classification/higgs/popper/show_results.py
@@ -6,11 +6,13 @@ import pandas as pd
 import glob
 
 list_reports = glob.glob("results/*/report.csv")
+dir_list = glob.glob("results/*")
 list_reports.sort()
+dir_list.sort()
 
 results = pd.read_csv(list_reports[-1], sep=",")
 
 sns.barplot(x = 'library', y = 'time', data = results)
 plt.title('Performance of the libraries with HIGGS dataset')
-plt.savefig("report.png")
+plt.savefig(dir_list[-1] + "/report.png")
 plt.show()
