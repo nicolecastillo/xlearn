1d8aa0d68fc4d049656de132ec3b427c37ceb742
Nicole Castillo <nicole.castillomac@gmail.com>
2020-06-30 18:47:23 -0700

    add save figure to show_results.py

--- demo/classification/higgs/popper/show_results.py
+++ demo/classification/higgs/popper/show_results.py
@@ -3,12 +3,14 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-import glob 
+import glob
 
-dirlist = glob.glob("results/*/report.csv")
+list_reports = glob.glob("results/*/report.csv")
+list_reports.sort()
 
-results = pd.read_csv(dirlist[-1], sep=",")
+results = pd.read_csv(list_reports[-1], sep=",")
 
 sns.barplot(x = 'library', y = 'time', data = results)
 plt.title('Performance of the libraries with HIGGS dataset')
+plt.savefig("report.png")
 plt.show()
